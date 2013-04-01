80c04423e758033ad39b7db6649868b838a6628d
Arthur Uzulin <fuj1nman@hotmail.com>
2013-04-01 20:51:36 +1100

    Working on a new block(s)
    Added render handler for a new block(s)

--- ClientProxyModJam.java
+++ ClientProxyModJam.java
@@ -8,18 +8,22 @@ public class ClientProxyModJam extends CommonProxyModJam{
     public static int awesomeOreRenderType;
     public static int chairRenderType;
     public static int tableRenderType;
+    public static int awesomeBlockRenderType;
     public static int awesomeOreRenderStage = 0;
     public static int chairRenderStage = 0;
     public static int tableRenderStage = 0;
+    public static int awesomeBlockRenderStage = 0;
     
 	@Override
 	public void handler(){
 		awesomeOreRenderType = RenderingRegistry.getNextAvailableRenderId();
 		chairRenderType = RenderingRegistry.getNextAvailableRenderId();
 		tableRenderType = RenderingRegistry.getNextAvailableRenderId();
+		awesomeBlockRenderType = RenderingRegistry.getNextAvailableRenderId();
         RenderingRegistry.registerBlockHandler(new RenderAwesomeOre());
         RenderingRegistry.registerBlockHandler(new RenderChair());
         RenderingRegistry.registerBlockHandler(new RenderTable());
+        RenderingRegistry.registerBlockHandler(new RenderAwesomeBlock());
         awesomeArmorID = RenderingRegistry.addNewArmourRendererPrefix("awesome");
 	}
 }
