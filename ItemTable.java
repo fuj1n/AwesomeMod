3e388baf057e34d17d1ac11a6b18581ae34fbb24
Arthur Uzulin <fuj1nman@hotmail.com>
2013-04-05 22:48:37 +1100

    Added item rendering for tables
    Tables will now render 3D in inventory.

--- ItemTable.java
+++ ItemTable.java
@@ -1,13 +1,9 @@
 package modJam;
 
-import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Icon;
 
 public class ItemTable extends ItemBlock{
-
-	private Icon[] tableOverlay = new Icon[16];
 	
 	private static final String[] subNames = { 
 		"white", "orange", "magenta",
@@ -21,15 +17,6 @@ public class ItemTable extends ItemBlock{
 	}
 	
     /**
-     * Returns 0 for /terrain.png, 1 for /gui/items.png
-     */
-	@Override
-    public int getSpriteNumber()
-    {
-        return 1;
-    }
-	
-    /**
      * Returns the metadata of the block which this Item (ItemBlock) can place
      */
     public int getMetadata(int par1)
@@ -40,57 +27,6 @@ public class ItemTable extends ItemBlock{
     @Override
     public String getUnlocalizedName(ItemStack par1ItemStack){
     	return getUnlocalizedName() + "." + subNames[par1ItemStack.getItemDamage()];
-    }
-    
-	@Override
-	public boolean requiresMultipleRenderPasses(){
-		return true;
-	}
-    
-    /**
-     * Gets an icon index based on an item's damage value and the given render pass
-     */
-    @Override
-    public Icon getIconFromDamageForRenderPass(int par1, int par2)
-    {
-    	if(par2 == 0)
-    		return this.iconIndex;
-    	else if(par2 == 1){
-    		return this.tableOverlay[par1];
-    	}else{
-    		return this.iconIndex;
-    	}
-    }
-    
-    public void updateIcons(IconRegister par1IconRegister){
-    	int tableType = this.getBlockID() == ModJam.woodTable.blockID ? 0 : 1;
-    	switch(tableType){
-    	case 0:
-    		this.iconIndex = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.tableWood");
-    		break;
-    	case 1:
-    		this.iconIndex = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.tableStone");
-    		break;
-    	default:
-    		this.iconIndex = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.tableWood");
-    		break;
-    	}
-    	this.tableOverlay[0] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableWhite");
-    	this.tableOverlay[1] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableOrange");
-    	this.tableOverlay[2] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableMagenta");
-    	this.tableOverlay[3] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableLBlue");
-    	this.tableOverlay[4] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableYellow");
-    	this.tableOverlay[5] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableLime");
-    	this.tableOverlay[6] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTablePink");
-    	this.tableOverlay[7] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableGray");
-    	this.tableOverlay[8] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableLGray");
-    	this.tableOverlay[9] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableCyan");
-    	this.tableOverlay[10] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTablePurple");
-    	this.tableOverlay[11] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableBlue");
-    	this.tableOverlay[12] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableBrown");
-    	this.tableOverlay[13] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableGreen");
-    	this.tableOverlay[14] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableRed");
-    	this.tableOverlay[15] = par1IconRegister.registerIcon("awesomeMod:fuj1n.AwesomeMod.overlayTableBlack");
     }
 
 }
--- RenderTable.java
+++ RenderTable.java
@@ -1,7 +1,10 @@
 package modJam;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
@@ -10,6 +13,89 @@ public class RenderTable implements ISimpleBlockRenderingHandler {
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID,
 			RenderBlocks renderer) {
+		BlockTable handlerBlock = (BlockTable)block;
+		GL11.glTranslatef(-0.5F, -0.5F, -0.5F);
+		Tessellator tessellator = Tessellator.instance;
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(-1F, 0F, 0F);
+		renderer.setRenderBounds(0.001, 0.001, 0.001, 0.099, 0.899, 0.099);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderEastFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderWestFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderTopFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.setRenderBounds(0, 0, 0, 0.1, 0.9, 0.1);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderEastFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderWestFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderTopFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.setRenderBounds(0.901, 0.001, 0.001, 0.999, 0.899, 0.099);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderEastFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderWestFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderTopFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.setRenderBounds(0.9, 0, 0, 1, 0.9, 0.1);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderEastFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderWestFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderTopFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(+1F, 0F, 0F);
+		renderer.setRenderBounds(0.001, 0.001, 0.901, 0.099, 0.899, 0.999);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderEastFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderWestFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderTopFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.setRenderBounds(0.0, 0.0, 0.9, 0.1, 0.9, 1.0);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderEastFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderWestFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderTopFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.setRenderBounds(0.901, 0.001, 0.901, 0.999, 0.899, 0.999);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderEastFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderWestFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderTopFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.setRenderBounds(0.9, 0.0, 0.9, 1.0, 0.9, 1.0);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderEastFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderWestFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderTopFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(0F, +1F, 0F);
+		renderer.setRenderBounds(0.001, 0.899, 0.001, 0.999, 0.999, 0.999);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderEastFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderWestFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderTopFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, handlerBlock.belowBlock.getBlockTextureFromSide(0));
+		renderer.setRenderBounds(0F, 0.9F, 0F, 1F, 1F, 1F);
+		renderer.renderNorthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderEastFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderSouthFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderWestFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderTopFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		renderer.renderBottomFace(block, 0D, 0D, 0D, block.getBlockTextureFromSideAndMetadata(0, metadata));
+		tessellator.draw();
+		GL11.glTranslatef(+0.5F, +0.5F, +0.5F);
 	}
 
 	@Override
@@ -63,7 +149,7 @@ public class RenderTable implements ISimpleBlockRenderingHandler {
 
 	@Override
 	public boolean shouldRender3DInInventory() {
-		return false;
+		return true;
 	}
 
 	@Override
