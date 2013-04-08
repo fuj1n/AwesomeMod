4a4b6efaacc4492708870f882e29a95e07a36e9d
Arthur Uzulin <fuj1nman@hotmail.com>
2013-04-08 17:08:45 +1000

    Update javadoc comments

--- ContainerDummy.java
+++ ContainerDummy.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 
 /**
+ * COPY INFORMATION
+ * 
  * This is a dummy container created for instances of GuiContainer that do not use any slots,
  * this class has no other uses, and therefore must be treated as a redundant class. 
  */
