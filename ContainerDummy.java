093860e18f4f912c1a05c49f6f2e6f3c9df4c1b6
Arthur Uzulin <fuj1nman@hotmail.com>
2013-04-08 17:07:15 +1000

    Update javadoc comments.

--- ContainerDummy.java
+++ ContainerDummy.java
@@ -3,6 +3,11 @@ package modJam;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 
+/**
+ * This is a dummy container created for instances of GuiContainer that do not use any slots,
+ * this class has no other uses, and therefore must be treated as a redundant class. 
+ */
+
 public class ContainerDummy extends Container{
 
 	public boolean isGuiAvailable = true;
